6a2ce6e98ef9c07e56f465d357f8cd4ee7a14df5
Shubham762 <tiwarishubham762@gmail.com>
2025-03-29 15:37:10 +0530

    exploring get ,post ,delete

--- src/app.js
+++ src/app.js
@@ -1,15 +1,48 @@
 const express=require('express');
 const app=express();
 
-app.use("/",(req,res)=>{
-    res.send("Hello from varanasi dashboard🙏!")
+// app.use("/",(req,res)=>{
+//     res.send("Hello from varanasi dashboard🙏!")
+// })
+
+// app.use("/test/2",(req,res)=>{
+//     res.send("abcd")
+// })
+// app.use("/user",(req,res)=>{
+//     res.send("Hello world!")
+// })
+// app.use("/hello",(req,res)=>{
+//     res.send("Hello hello hello!✌️")
+// })
+// app.listen(3000,()=>{
+//     console.log("server is running on port 3000");
+// })
+
+// app.use("/",(req,res)=>{
+//     res.send("Hello from varanasi dashboard🙏!")
+// })
+
+app.get("/user",(req,res)=>{
+    res.send({
+        firstName:"Shubham",
+        lastName:"Tiwari"
+    })
+})
+app.post("/user",(req,res)=>{
+   // console.log("data saved successfully")
+  res.send("Data is saved successfully in the database")
+})
+app.delete("/user",(req,res)=>{
+    res.send("Data deleted successfully!😍")
+})
+app.use("/user",(req,res)=>{
+    res.send("Hello world!")
 })
 app.use("/test",(req,res)=>{
     res.send("Hello world!")
 })
-app.use("/hello",(req,res)=>{
-    res.send("Hello hello hello!✌️")
-})
-app.listen(3000,()=>{
-    console.log("server is running on port 3000");
+
+
+app.listen(7777,()=>{
+    console.log("listenng 7777 successfully ...")
 })
