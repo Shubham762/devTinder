f46f18c418d540391f4938a41ba80e0810e25394
Shubham762 <tiwarishubham762@gmail.com>
2025-04-14 20:47:40 +0530

    Error-handling

--- src/app.js
+++ src/app.js
@@ -16,6 +16,17 @@ app.get("/admin/deleteUser",(req,res)=>{
     res.send("Deleted the user");
 })
 
+app.get("/getUserData",(req,res)=>{
+  // throw new Error;
+   res.send("user data sent")
+})
+app.get("/",(error,req,res,next)=>{ ///oder of the parameter matteres here
+   if(error){
+    res.status(500).send("Something went wrong");
+   }
+
+ })
+
 app.listen(7777,()=>{
     console.log("listenng 7777 successfully ...");
 })
